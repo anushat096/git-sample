ca95b5158ac1066021f2024b9bbf0b3a3b098c46
anushat096 <anushat096@gmail.com>
2022-04-29 13:26:22 +0530

    added user data

--- user_data.py
+++ user_data.py
@@ -3,6 +3,5 @@ from base import session_factory
 
 session = session_factory()
 adarsh = User(**dict(user_name="adarsh",first_name="Adarsh", last_name="Ramalingaiah", email="adakr@deloitte.com"))
-anusha = User(**dict(user_name="anusha",first_name="Anusha",last_name="Shetty", email="anusha@gmail.com"))
-
+anusha1 = User(**dict(user_name="anusha",first_name="Anusha",last_name="Shetty", email="anusha@gmail.com"))
 
